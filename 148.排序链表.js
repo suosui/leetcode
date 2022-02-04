e0f2401770863f51c03aa94287c976b247452001
suosui <18844141934@163.com>
2022-02-04 22:19:31 +0800

    148.排序链表

---
+++ 148.排序链表.js
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=javascript
+ *
+ * [148] 排序链表
+ *
+ * https://leetcode-cn.com/problems/sort-list/description/
+ *
+ * algorithms
+ * Medium (66.61%)
+ * Likes:    1449
+ * Dislikes: 0
+ * Total Accepted:    242.3K
+ * Total Submissions: 363.7K
+ * Testcase Example:  '[4,2,1,3]'
+ *
+ * 给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [4,2,1,3]
+ * 输出：[1,2,3,4]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [-1,5,3,4,0]
+ * 输出：[-1,0,3,4,5]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目在范围 [0, 5 * 10^4] 内
+ * -10^5 <= Node.val <= 10^5
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+    let cur = head;
+    while (cur) {
+        let next = cur.next;
+        while(next){
+            if(next.val < cur.val){
+                let tmp = next.val;
+                next.val = cur.val;
+                cur.val = tmp;
+            }
+            next = next.next;
+        }
+        cur = cur.next;
+    }
+    return head;
+};
+// @lc code=end
+
