64fe910528631caf1ae5939338adaa1a899b70fa
suosui <18844141934@163.com>
2022-10-29 21:07:59 +0800

    713.乘积小于-k-的子数组

---
+++ subjects/713.乘积小于-k-的子数组.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode.cn id=713 lang=javascript
+ *
+ * [713] 乘积小于 K 的子数组
+ *
+ * https://leetcode.cn/problems/subarray-product-less-than-k/description/
+ *
+ * algorithms
+ * Medium (48.88%)
+ * Likes:    612
+ * Dislikes: 0
+ * Total Accepted:    88.2K
+ * Total Submissions: 180.2K
+ * Testcase Example:  '[10,5,2,6]\n100'
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，请你返回子数组内所有元素的乘积严格小于 k 的连续子数组的数目。
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [10,5,2,6,7], k = 100
+ * 输出：8
+ * 解释：8 个乘积小于 100 的子数组分别为：[10]、[5]、[2],、[6]、[10,5]、[5,2]、[2,6]、[5,2,6]。
+ * 需要注意的是 [10,5,2] 并不是乘积小于 100 的子数组。
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [1,2,3], k = 0
+ * 输出：0
+ * 
+ * 
+ * 
+ * 提示: 
+ * 
+ * 
+ * 1 <= nums.length <= 3 * 10^4
+ * 1 <= nums[i] <= 1000
+ * 0 <= k <= 10^6
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var numSubarrayProductLessThanK = function (nums, k) {
+    let n = nums.length, ret = 0;
+    let prod = 1, i = 0;
+    for (let j = 0; j < n; j++) {
+        prod *= nums[j];
+        while (i <= j && prod >= k) {
+            prod /= nums[i];
+            i++;
+        }
+        ret += j - i + 1;
+    }
+    return ret;
+};
+// @lc code=end
+
