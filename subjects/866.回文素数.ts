dbab6b80aefbf0ee29f7583887f4ddf484d9a46a
suosui <18844141934@163.com>
2022-11-05 15:11:42 +0800

    fomat

--- subjects/866.回文素数.ts
+++ subjects/866.回文素数.ts
@@ -6,32 +6,32 @@
 
 // @lc code=start
 const primePalindrome = (n: number): number => {
-  if (n === 1) return 2;
-  let cusor = n;
-  while (cusor < 2 * Math.pow(10, 8)) {
-    const len = cusor.toString().length;
-    if (len % 2 == 0 && cusor != 10 && cusor != 11) {
-      cusor = Math.pow(10, len);
+    if (n === 1) return 2;
+    let cusor = n;
+    while (cusor < 2 * Math.pow(10, 8)) {
+        const len = cusor.toString().length;
+        if (len % 2 == 0 && cusor != 10 && cusor != 11) {
+            cusor = Math.pow(10, len);
+        }
+        if (isPalindromePrime(cusor) && isPrime(cusor)) {
+            return cusor;
+        }
+        cusor++;
     }
-    if (isPalindromePrime(cusor) && isPrime(cusor)) {
-      return cusor;
-    }
-    cusor++;
-  }
 };
 
 const isPrime = (n: number): boolean => {
-  for (let i = 2; i * i <= n; i++) {
-    if (n % i === 0) {
-      return false;
+    for (let i = 2; i * i <= n; i++) {
+        if (n % i === 0) {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 
 const isPalindromePrime = (n: number): boolean => {
-  return Number(String(n).split("").reverse().join("")).valueOf() === n.valueOf();
+    return Number(String(n).split("").reverse().join("")).valueOf() === n.valueOf();
 }
 // @lc code=end
 
