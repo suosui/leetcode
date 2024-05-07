d107ac542491acc1140473a01aee1377d608a433
suosui <18844141934@163.com>
2024-05-07 16:43:30 +0800

    add: solution for 26.删除有序数组中的重复项

--- subjects/26.删除有序数组中的重复项.js
+++ subjects/26.删除有序数组中的重复项.js
@@ -2,6 +2,36 @@
  * @lc app=leetcode.cn id=26 lang=javascript
  *
  * [26] 删除有序数组中的重复项
+ *  方法一: 双指针
+ *  过程: 
+ *     a.设置left,right指针，数组下标0至left始终是不重复的元素。
+ *     b.如果 nums[left] === nums[right] 说明有重复的元素，让right++直到nums[left] !== nums[right]。因为是递增关系，所以这时的nums[right]是下一个不重复的元素。
+ *     c. nums[left+1] = nums[right] && left++。
+ *     d. 直到left<nums.length 或 right<nums.length。
+ *  复杂度:
+ *     时间复杂度: O(n)。
+ *     空间复杂度: O(1)。
+ *  代码:
+ *      var removeDuplicates = function (nums) {
+ *        let left = 0;
+ *        let right = left + 1;
+ *        while (left < nums.length && right < nums.length) {
+ *            if (nums[left] === nums[right]) {
+ *                while (nums[left] === nums[right]) {
+ *                    right++;
+ *                }
+ *                if (right >= nums.length) return left + 1;
+ *                nums[left + 1] = nums[right];
+ *                left++;
+ *                right++;
+ *            } else {
+ *                nums[left + 1] = nums[right];
+ *                left++;
+ *                continue;
+ *            }
+ *        }
+ *        return left + 1;
+ *      };
  */
 
 // @lc code=start
@@ -10,21 +40,5 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    if(nums.length ===0 ) return 0;
-    let len = 1;
-    let j = 1;
-    for (let i = 0; i < nums.length; i++) {
-        while (j < nums.length) {
-            if (nums[j] === nums[i]) {
-                j ++;
-                continue;
-            }
-            nums[i+1] = nums[j];
-            len ++;
-            break;
-        }
-
-    }
-    return len;
 };
 // @lc code=end
