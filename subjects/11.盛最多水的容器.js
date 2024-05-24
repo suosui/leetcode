72c5bd4e506e96daba8d1e8ddc1b54aa73bd7408
suosui <18844141934@163.com>
2024-05-24 16:40:55 +0800

    11 盛最多水的容器

--- subjects/11.盛最多水的容器.js
+++ subjects/11.盛最多水的容器.js
@@ -55,34 +55,45 @@
 /**
  * @param {number[]} height
  * @return {number}
+ * 
+ * 方法一：双指针
+ * 思路：
+ *    在初始时，左右指针分别指向数组的左右两端，它们可以容纳的水量为 min⁡(1,7)∗8=8。
+ *    此时我们需要移动一个指针。移动哪一个呢？直觉告诉我们，应该移动对应数字较小的那个指针（即此时的左指针）。这是因为，由于容纳的水量是由
+ *    两个指针指向的数字中较小值∗指针之间的距离两个指针指向的数字中较小值 * 指针之间的距离
+ *    两个指针指向的数字中较小值∗指针之间的距离
+ *    决定的。如果我们移动数字较大的那个指针，那么前者「两个指针指向的数字中较小值」不会增加，后者「指针之间的距离」会减小，那么这个乘积会减小。
+ *    因此，我们移动数字较大的那个指针是不合理的。因此，我们移动 数字较小的那个指针。
+ *    
+ *    作者：力扣官方题解
+ *    链接：https://leetcode.cn/problems/container-with-most-water/solutions/207215/sheng-zui-duo-shui-de-rong-qi-by-leetcode-solution/
+ *    来源：力扣（LeetCode）
+ *    著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * 复杂度：
+ *     时间复杂度：O(n)
+ *     空间复杂度：O(1)
+ * 代码：
+ *     var maxArea = function(height) {
+ *         let right = height.length-1;
+ *         let left = 0;
+ *         let max = 0;
+ *         while(right > left){
+ *             let h = height[right] > height[left] ?  height[left] : height[right];
+ *             let w = right-left;
+ *             if(h * w > max){
+ *                 max = h*w;
+ *             }
+ *             if(height[right] > height[left]){
+ *                 left ++;
+ *             } else {
+ *                 right--;
+ *             }
+ *         }
+ *         return max;
+ *     };
  */
 var maxArea = function (height) {
-    let max = 0;
-    let pointerX = 0;
-    let pointerY = height.length - 1;
-    while (pointerX < pointerY) {
-        const area = calArea(height[pointerX], height[pointerY], pointerX, pointerY);
-        if (area > max) {
-            max = area;
-        }
-        if (height[pointerX] >= height[pointerY]) {
-            pointerY--;
-        } else {
-            pointerX++;
-        }
-    }
-    return max
+
 };
-
-var calArea = (x, y, idxX, idxY) => {
-    if (x < y) {
-        return Math.abs(idxX - idxY) * x
-    } else if (x > y) {
-        return Math.abs(idxX - idxY) * y
-    }
-    return Math.abs(idxX - idxY) * y
-}
-
-
 // @lc code=end
 
