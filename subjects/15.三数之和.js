ed17b2d2d015a183ec3549e5834288ccc76b8ee6
suosui <18844141934@163.com>
2024-05-27 16:20:11 +0800

    15. 三数之和

--- subjects/15.三数之和.js
+++ subjects/15.三数之和.js
@@ -67,42 +67,58 @@
 // @lc code=start
 /**
  * @param {number[]} nums
- * @return {number[][]}
+ * @return {number[][]}\
+ * 
+ *  方法一：排序 + 双指针（两数之和）
+ *  思路：
+ *     可以把三数之和转化为两数之和，即固定一个数，找到另外两个数的和等于第一个数的相反数。
+ *     因为对数组进行了排序，所以可以使用双指针来查找另外两个数。
+ *     怎么避免重复的三元组呢？可以在遍历的时候跳过重复的元素。（因为数组已经排序，所以重复的元素一定是相邻的）
+ *     1. 首先对数组进行排序，排序后固定一个数 nums[i]，再使用左右指针指向 nums[i]后面的两端，数字分别为 nums[L] 和 nums[R]，计算三个数的和 sum 判断是否满足为 0，满足则添加进结果集
+ *     2. 如果 nums[i]大于 0，则三数之和必然无法等于 0，结束循环
+ *     3. 如果 nums[i] == nums[i-1]，则说明该数字重复，会导致结果重复，所以应该跳过
+ *     4. 当 sum == 0 时，nums[L] == nums[L+1] 则会导致结果重复，应该跳过，L++
+ *     5. 当 sum == 0 时，nums[R] == nums[R-1] 则会导致结果重复，应该跳过，R--
+ *  复杂度：
+ *    时间复杂度：O(n^2)，n 为数组长度
+ *    空间复杂度：O(1)
+ *  代码：
+ *      var threeSum = function (nums) {
+ *          nums.sort((a, b) => { return a - b; });
+ *          let toReturn = [];
+ *          let startIdx = 0;
+ *          while (startIdx < nums.length - 2) {
+ *              let l = startIdx + 1;
+ *              let r = nums.length - 1;
+ *              let target = 0 - nums[startIdx];
+ *              if (startIdx > 0 && nums[startIdx] === nums[startIdx - 1]) {
+ *                  startIdx++;
+ *                  continue;
+ *              }
+ *              while (l < r) {
+ *                  if (target < nums[l] + nums[r]) {
+ *                      r--;
+ *                  } else if (target === nums[l] + nums[r]) {
+ *                      toReturn.push([nums[startIdx], nums[l], nums[r]]);
+ *                      while (nums[l + 1] === nums[l] && l < r) {
+ *                          l++;
+ *                      }
+ *                      while (nums[r - 1] === nums[r] && l < r) {
+ *                          r--;
+ *                      }
+ *                      l++;
+ *                      r--;
+ *                  } else {
+ *                      l++;
+ *                  }
+ *              }
+ *              startIdx++;
+ *          }
+ *          return toReturn;
+ *      };
  */
 var threeSum = function (nums) {
-    const results = [];
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length; i++) {
-        let pointerLeft = i + 1;
-        let pointerRight = nums.length - 1;
-        while (pointerLeft < nums.length && pointerLeft < pointerRight) {
-            let sum = nums[i] + nums[pointerLeft] + nums[pointerRight];
-            if (sum === 0) {
-                if (results.length > 0) {
-                    if (results[results.length - 1][1] === nums[pointerLeft] && results[results.length - 1][2] === nums[pointerRight]) {
-                        pointerLeft++;
-                        pointerRight--;
-                        continue;
-                    }
-                }
-                results.push([nums[i], nums[pointerLeft], nums[pointerRight]]);
-                while (i < nums.length) {
-                    if (nums[i + 1] === nums[i]) {
-                        i++;
-                    } else {
-                        break;
-                    }
-                }
-                pointerLeft++;
-                pointerRight--;
-            } else if (sum > 0) {
-                pointerRight--;
-            } else {
-                pointerLeft++;
-            }
-        }
-    }
-    return results;
+
 };
 // @lc code=end
 
