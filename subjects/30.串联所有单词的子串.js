6b4b189d6fc9751de3d48d300c9a1bbe5ef5ffea
suosui <18844141934@163.com>
2024-05-29 13:15:26 +0800

    30. 串联所有单词的子串

--- subjects/30.串联所有单词的子串.js
+++ subjects/30.串联所有单词的子串.js
@@ -75,29 +75,84 @@
  * @param {string} s
  * @param {string[]} words
  * @return {number[]}
+ * 
+ *  方法一：滑动窗口+哈希表
+ *  思路：
+ *     1. 首先判断s的长度是否小于words中所有单词的长度之和，如果小于直接返回[]
+ *     2. 用哈希表hasMap存储words中所有单词的出现次数
+ *     3. 用l和r两个指针，r指针每次移动一个单词的长度，判断r-l是否等于words中所有单词的长度之和
+ *     4. 如果等于，说明找到了一个符合条件的子串，将l加入结果集，然后重置hasMap，l和r指针
+ *     5. 获取r指针下一个单词，如果在hasMap中，且出现次数大于0，r指针继续移动，否则重置hasMap，l和r指针
+ *     6. 如果不在hasMap中，重置hasMap，l和r指针
+ *     7. 返回结果集
+ *  复杂度：
+ *     时间复杂度：O(n)
+ *     空间复杂度：O(n)
+ *  代码：
+ *      var findSubstring = function (s, words) {
+ *          if (s.length < words[0].length * words.length) {
+ *              return []
+ *          }
+ *          let hasMap = {};
+ *          for (let word of words) {
+ *              if (!hasMap[word]) {
+ *                  hasMap[word] = {
+ *                      cnt: 0,
+ *                      originCnt: 0,
+ *                  }
+ *              }
+ *              hasMap[word].cnt++;
+ *              hasMap[word].originCnt++;
+ *          }
+ *          let toReturn = [];
+ *          let l = 0;
+ *          let r = 0;
+ *          while (r <= s.length) {
+ *              if (r - l === words[0].length * words.length) {
+ *                  toReturn.push(l);
+ *                  resetHasMap(hasMap);
+ *                  l = r = l + 1;
+ *              }
+ *              let nextWord = getNextWord(r, s, words[0].length);
+ *              if (hasMap[nextWord]) {
+ *                  if (hasMap[nextWord].cnt > 0) {
+ *                      hasMap[nextWord].cnt--;
+ *                      r += words[0].length;
+ *                  } else {
+ *                      resetHasMap(hasMap);
+ *                      l = r = l + 1;
+ *                  }
+ *              } else {
+ *                  resetHasMap(hasMap);
+ *                  l = r = l + 1;
+ *              }
+ *          }
+ *          return toReturn;
+ *      };
+ *      
+ *      var resetHasMap = (hasMap) => {
+ *          for (let key in hasMap) {
+ *              hasMap[key].cnt = hasMap[key].originCnt;
+ *          }
+ *      }
+ *      
+ *      var getNextWord = (startIdx, s, length) => {
+ *          let word = '';
+ *          let i = startIdx;
+ *          if (startIdx + length > s.length) {
+ *              return word;
+ *          }
+ *          let cnt = 0;
+ *          while (cnt < length) {
+ *              word += s[i];
+ *              i++;
+ *              cnt++;
+ *          }
+ *          return word;
+ *      }
  */
 var findSubstring = function (s, words) {
-    const wordLength = words.length * words[0].length;
-    const res = [];
-    const wordJoined = words.sort().join('');
-    for (let i = 0; i < s.length - wordLength + 1; i++) {
-        const strChunks = chunk(s.slice(i, i + wordLength), words[0].length);
-        const strJoined = strChunks.sort().join('');
-        if (strJoined === wordJoined) {
-            res.push(i);
-        }
-    }
-    return res;
-};
 
-var chunk = function (s, length) {
-    const res = [];
-    let start = 0;
-    while (start <= s.length - length) {
-        res.push(s.slice(start, start + length));
-        start += length;
-    }
-    return res;
-}
+};
 // @lc code=end
 
