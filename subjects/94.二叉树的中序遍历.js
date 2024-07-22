4f129f09049228aa330c252342941543a032dc3e
suosui <18844141934@163.com>
2024-07-22 16:48:24 +0800

    94. 二叉树的中序遍历

--- subjects/94.二叉树的中序遍历.js
+++ subjects/94.二叉树的中序遍历.js
@@ -65,19 +65,52 @@
 /**
  * @param {TreeNode} root
  * @return {number[]}
+ * 
+ *  方法一：递归
+ *  思路：递归遍历左子树，访问根节点，递归遍历右子树
+ *  复杂度：
+ *       时间复杂度：O(n)，n为二叉树的节点个数
+ *       空间复杂度：O(n)，递归调用的栈空间
+ *  代码：
+ *       var inorderTraversal = function (root) {
+ *           const res = [];
+ *           var inorder = (root) => {
+ *               if (!root) {
+ *                   return;
+ *               }
+ *               inorder(root.left);
+ *               res.push(root.val);
+ *               inorder(root.right);
+ *           }
+ *           inorder(root);
+ *           return res;
+ *       }; 
+ *  
+ *  方法二：迭代
+ *  思路：使用栈模拟递归过程，模拟递归过程的调用栈
+ *  复杂度：
+ *       时间复杂度：O(2n)，n为二叉树的节点个数
+ *       空间复杂度：O(n)，栈空间
+ *  代码：
+ *       var inorderTraversal = function (root) {
+ *           const res = [];
+ *           const stack = [];
+ *           if(!root) return res;
+ *           stack.push({ color: 'WHITE', node: root });
+ *           while (stack.length) {
+ *               const { color, node } = stack.pop();
+ *               if (color === 'WHITE') {
+ *                   if (node.right) stack.push({ color: 'WHITE', node: node.right });
+ *                   stack.push({ color: 'GREY', node });
+ *                   if (node.left) stack.push({ color: 'WHITE', node: node.left });
+ *               } else if (color === 'GREY') {
+ *                   res.push(node.val);
+ *               }
+ *           }
+ *           return res;
+ *       };
  */
 var inorderTraversal = function (root) {
-    const res = [];
-    var inorder = (root) => {
-        if (!root) {
-            return;
-        }
-        inorder(root.left);
-        res.push(root.val);
-        inorder(root.right);
-    }
-    inorder(root);
-    return res;
 };
 // @lc code=end
 
